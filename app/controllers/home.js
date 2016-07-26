8ea123cf217e67e32676953651465fedb44fade5
felipe <fesntmail@gmail.com>
2016-07-26 13:32:18 -0300

    Adiciona Last-Modified header

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -11,12 +11,16 @@ module.exports = function (app) {
   app.use('/', router);
 };
 
+last_date = new Date();
+
 router.get('/disciplinas', function (req, res, next) {
   res.charset = 'ISO-8859-1';
+  res.header('Last-Modified', last_date);
   res.sendFile(path.join(__dirname, '../../build/tmp', 'processed.json'));
 });
 
 router.get('/horarios', function (req, res, next) {
   res.charset = 'ISO-8859-1';
+  res.header('Last-Modified', last_date);
   res.sendFile(path.join(__dirname, '../../build/tmp', 'horarios.json'));
 });
